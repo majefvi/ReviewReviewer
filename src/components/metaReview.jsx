8f24ef79921b3440f114b4051d82c66f8c40b7d4
Jeffrey J. Haywood <jeffreyjhaywood@gmail.com>
2020-07-20 11:08:50 -0400

    Add components for Meta and Original review.

---
+++ src/components/metaReview.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class MetaReview extends Component {
+  state = {};
+  render() {
+    return <React.Fragment></React.Fragment>;
+  }
+}
+
+export default MetaReview;
---
+++ src/components/originalReview.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class OriginalReview extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">Test</h5>
+            <h6 className="card-subtitle mb-2 text-muted">Test</h6>
+            <p className="card-text">Test</p>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default OriginalReview;
