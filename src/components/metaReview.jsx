55068901d2e4520b34b574e44564437669433b24
Jeffrey J. Haywood <jeffreyjhaywood@gmail.com>
2020-07-22 11:46:03 -0400

    Add onChange to input forms. Change HTML title

--- src/components/metaReview.jsx
+++ src/components/metaReview.jsx
@@ -1,18 +1,18 @@
 import React, { Component } from "react";
 import MetaReviewForm from "./metaReviewForm";
-import { Card } from "@material-ui/core";
-import { CardHeader } from "@material-ui/core";
-import { CardContent } from "@material-ui/core";
+import { Card, CardContent, CardHeader } from "@material-ui/core";
 
 class MetaReview extends Component {
   render() {
     return (
-      <Card style={{ position: "relative" }}>
-        <CardHeader title="Review this review!"></CardHeader>
-        <CardContent>
-          <MetaReviewForm></MetaReviewForm>
-        </CardContent>
-      </Card>
+      <React.Fragment>
+        <Card style={{ position: "relative" }}>
+          <CardHeader title="Review this review!"></CardHeader>
+          <CardContent>
+            <MetaReviewForm></MetaReviewForm>
+          </CardContent>
+        </Card>
+      </React.Fragment>
     );
   }
 }
--- src/components/metaReviewForm.jsx
+++ src/components/metaReviewForm.jsx
@@ -1,28 +1,98 @@
 import React, { Component } from "react";
 import { Rating } from "@material-ui/lab";
-import { FormControl, Input, InputLabel, TextField } from "@material-ui/core";
+import { FormControl, TextField, Button } from "@material-ui/core";
 
 class MetaReviewForm extends Component {
   state = {};
+
+  constructor() {
+    super();
+    this.state = { author: "", starRating: 0, starReview: "" };
+  }
+
+  handleNameChange = (event) => {
+    const userInput = event.target.value;
+    this.setState({
+      author: userInput,
+    });
+  };
+
+  handleRatingChange = (event) => {
+    const userInput = event.target.value;
+    this.setState({
+      starRating: userInput,
+    });
+  };
+
+  // Get data from all inputs and build object from it
+  handleReviewChange = (event) => {
+    const userInput = event.target.value;
+    this.setState({
+      review: userInput,
+    });
+  };
+
+  handleSubmitReview = (event) => {
+    event.preventDefault();
+    // alert(this.state.author);
+    // alert(this.state.starRating);
+    // alert(this.state.review);
+
+    let formState = this.state;
+    console.log("Form state: ", this.state);
+
+    this.postReview(formState);
+
+    // this.props.onSubmitReview(this.state.buffer);
+    this.setState({
+      author: "",
+      starRating: 0,
+      review: "",
+    });
+  };
+
+  postReview = async (formState) => {
+    console.log("Post initiated");
+    await fetch("/savemetareview", formState).then((err, res) => {
+      if (err) return alert("Data wasn't saved");
+      return alert("Successfully submitted");
+    });
+  };
+
   render() {
+    const { buffer } = this.state;
+
     return (
-      <FormControl>
-        <TextField
-          id="reviewer-name"
-          label="Name"
-          variant="outlined"
-        ></TextField>
+      <form>
+        <FormControl>
+          <TextField
+            id="reviewer-name"
+            label="Name"
+            variant="outlined"
+            required
+            onChange={this.handleNameChange}
+          ></TextField>
 
-        <Rating></Rating>
+          <Rating onChange={this.handleRatingChange}></Rating>
 
-        <TextField
-          id="meta-review-description"
-          label="Review"
-          multiline
-          rows={4}
-          variant="outlined"
-        ></TextField>
-      </FormControl>
+          <TextField
+            id="meta-review-description"
+            label="Review"
+            multiline
+            rows={4}
+            variant="outlined"
+            onChange={this.handleReviewChange}
+          ></TextField>
+
+          <Button
+            onClick={this.handleSubmitReview}
+            fullWidth
+            style={{ backgroundColor: "#43b581" }}
+          >
+            Submit Review
+          </Button>
+        </FormControl>
+      </form>
     );
   }
 }
