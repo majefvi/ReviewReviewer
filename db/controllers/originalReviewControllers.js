02ed41ce78bda6af8e2dedd8f2d495c9292881db
itsOrD <ord.sc2@gmail.com>
2020-07-23 08:34:42 -0700

    further work on separating db config from its individual connection points

--- db/controllers/originalReviewControllers.js
+++ db/controllers/originalReviewControllers.js
@@ -3,6 +3,7 @@ const customModels = require("../models/originalReviewModel.js");
 const randomReview = () =>
   customModels.OriginalReview.find({})
     .limit(1)
+    // .then((data) => res.send(data))
     .then((data) => console.log("here is our find data: ", data))
     .catch((err) => console.log("something is wrong: ", err));
 
