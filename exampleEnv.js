9472475786d60781be5162859839ed6f3a25d5b0
Matthew Beckerleg <matthew.beckerleg@gmail.com>
2020-07-22 18:35:05 -0700

    Revert "Db branch"

--- exampleEnv.js
+++
@@ -1,9 +0,0 @@
-// This project uses private Atlas Mongo cluster
-DB_HOST = localhost;
-// Private user utiliized
-DB_USER = root;
-// hidden
-DB_PASS = s1mpl3;
-
-// Replace <username>, <password>, and <dbname> with your own
-// CUSTOM_DB_URI=mongodb://<username>:<password>@test1-shard-00-00.3ywma.mongodb.net:27017,test1-shard-00-01.3ywma.mongodb.net:27017,test1-shard-00-02.3ywma.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-dws3a4-shard-0&authSource=admin&retryWrites=true&w=majority
